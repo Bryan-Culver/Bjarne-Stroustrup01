a462bf93f9f1be926fc42c936cafa918df4a912b
Bryan Culver <culver.bryan.t@gmail.com>
2020-11-03 11:41:12 -0500

    On Page 61 going through chapter 3.

--- HelloWorld/HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld/HelloWorld.cpp
@@ -1,15 +1,18 @@
-/* HelloWorld.cpp: This is an introductory program from chapter 2. 
+/* HelloWorld.cpp: This is an introductory program from chapter 2, and continued 
+ * in Chapter 3.
  * Author: Bryan Culver
  * Date created: 11/02/2020
- * Version: 0.1 11/02/2020
+ * Version: 1.1 11/03/2020
  */
 
 #include "../../std_lib_facilities.h"
 
 int main()
 {
-	cout << "Hello, World!\n";	// prints to output
-	keep_window_open();			// waits for input
+	cout << "Please enter your first name (followed by 'enter'):\n";	// prints to output
+	string first_name; 			// first_name is a variable of type string
+	cin >> first_name;			// reads characters into first_name
+	cout >> "Hello, " << first_name << "!\n";
 	return 0;
 }
 
