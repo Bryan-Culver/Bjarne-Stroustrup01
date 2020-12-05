4d26baac6c3b27733920cd61c22abdc7df0757f6
Bryan Culver <culver.bryan.t@gmail.com>
2020-12-04 22:32:06 -0500

    Able to create a 'hello world gui window.

--- Chapter12/triangle.cpp
+++ Chapter12/triangle.cpp
@@ -1,5 +1,5 @@
-#include "Simple_window.h"
-#include "Graph.h"
+#include "../Simple_window.h"
+#include "../Graph.h"
 
 int main(){
     using namespace Graph_lib;
@@ -7,5 +7,10 @@ int main(){
     Point tl(100, 100);
     Simple_window win(tl,600,400,"Canvas");
     Polygon poly;
-    
+    poly.add(Point(300,200));
+    poly.add(Point(350,100));
+    poly.add(Point(400,200));
+    poly.set_color(Color::red);
+    win.attach(poly);
+    win.wait_for_button();    
 }
